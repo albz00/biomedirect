cba10b1961d47556bd74c775a42d2d6f77194df0
Alex Zimmerman <alzimmr1@gmail.com>
2025-11-16 20:50:26 -0500

    test

--- TextX/LessonsX/EmbryologyX/GestationalOverviewX/GestationalOverviewX.js
+++
@@ -1,4 +0,0 @@
-// Lesson Menu Link
-//$(function(){
-//    $('#menu1').on('click', function () { currentSlide = 0; clickedLink = true; }); // Play
-//});
--- TextX/LessonsX/EmbryologyX/TeratologyX/TeratologyX.js
+++
@@ -1,8 +0,0 @@
-// Lesson Menu Links
-$(function(){
-    $('#menu1').on('click', function () { currentSlide = 0; clickedLink = true; }); // Glossary
-    $('#menu2').on('click', function () { currentSlide = 3; clickedLink = true; }); // Principles
-    $('#menu3').on('click', function () { currentSlide = 4; clickedLink = true; }); // Susceptibility Periods
-    $('#menu4').on('click', function () { currentSlide = 5; clickedLink = true; }); // Categorization
-    $('#menu5').on('click', function () { currentSlide = 6; clickedLink = true; }); // Teratogens
-});
